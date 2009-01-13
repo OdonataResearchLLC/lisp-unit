47045b0eb5360f3d04e57deff432dbe322596504
Thomas M. Hermann <tmh.public@gmail.com>
2009-01-13 18:18:27 +0000

    [project @ Corrected ROUNDOFF-ERROR to always return the absolute value.]

--- lisp-unit.lisp
+++ lisp-unit.lisp
@@ -433,9 +433,9 @@ For more information, see lisp-unit.html.
 (defun roundoff-error (exact approximate)
   "Returned the error delta between the exact and approximate floating
 point value."
-  (if (or (= 0.0 exact) (= 0.0 approximate))
-      (+ exact approximate)
-      (abs (- (/ approximate exact) 1.0))))
+  (abs (if (or (= 0.0 exact) (= 0.0 approximate))
+	   (+ exact approximate)
+	   (- (/ approximate exact) 1.0))))
 
 ;;; (FLOAT-EQUAL x y &optional epsilon) => true or false
 ;;; Return true if the absolute difference between x and y is less
