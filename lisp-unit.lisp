8b67002234558b10ffd9fbcfdf4503477ab64e02
Ryan Davis <ryan@acceleration.net>
2013-01-31 13:07:58 -0500

    reverting some code from 57886a, taking an alternate approach.

--- lisp-unit.lisp
+++ lisp-unit.lisp
@@ -90,8 +90,6 @@ functions or even macros does not require reloading any tests.
            :print-failures
            :print-errors
            :summarize-results)
-  ;; behavioral parameters
-  (:export :*keep-passing-asserts*)
   ;; Utility predicates
   (:export :logically-equal :set-equal))
 
@@ -134,11 +132,6 @@ assertion.")
 (defun use-debugger (&optional (flag t))
   "Use the debugger when testing, or not."
   (setq *use-debugger* flag))
-
-(defparameter *keep-passing-asserts* T
-  "when non-nil, passing test assertions will be collected as objects and
-  accessible in test-result objects.  When nil, only the type of the passing
-  assertion will be collected, saving memory."  )
 
 ;;; Global unit test database
 
